fa6515dd0079bbbfb0da2d3a35d44c1ed31053e2
Liza <richardaddy333@gmail.com>
2023-01-13 12:51:05 +0300

    feat: add filtering for the first three tasks feat: move the filtering logic to todolist

---
+++ src/Todolist.tsx
@@ -0,0 +1,90 @@
+import React, {useState} from 'react';
+
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    removeTask: (taskId: number) => void
+    deleteAllTasks: () => void
+}
+
+type FilterValuesType = "all" | "active" | "completed" | "three";
+
+export function Todolist(props: PropsType) {
+
+    let [filter, setFilter] = useState<FilterValuesType>("all");
+    let tasksForTodolist = props.tasks;
+
+    if (filter === "active") {
+        tasksForTodolist = props.tasks.filter(t => t.isDone);
+    }
+    if (filter === "completed") {
+        tasksForTodolist = props.tasks.filter(t => !t.isDone);
+    }
+    if (filter === "three") {
+        tasksForTodolist = props.tasks.filter(t => t.id === 1 || t.id === 2 || t.id === 3);
+    }
+
+    function changeFilter(value: FilterValuesType) {
+        setFilter(value);
+    }
+
+
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {
+                    tasksForTodolist.map(t => <li key={t.id}>
+                            <input type="checkbox" checked={t.isDone}/>
+                            <span>{t.title}</span>
+                            <button onClick={() => {
+                                props.removeTask(t.id)
+                            }}>x
+                            </button>
+                        </li>
+                    )
+                }
+            </ul>
+            <div>
+                <button onClick={() => props.deleteAllTasks()}>
+                    DELETE ALL TASKS
+                </button>
+
+                <button onClick={() => {
+                    changeFilter("all")
+                }}>
+                    All
+                </button>
+
+                <button onClick={() => {
+                    changeFilter("active")
+                }}>
+                    Active
+                </button>
+
+                <button onClick={() => {
+                    changeFilter("completed")
+                }}>
+                    Completed
+                </button>
+
+                <button onClick={() => {
+                    changeFilter("three")
+                }}>
+                    First three tasks
+                </button>
+
+            </div>
+        </div>
+    )
+}
