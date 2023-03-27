66fb8d95b189003e7ecf86413e57085ec4f6be82
Liza <richardaddy333@gmail.com>
2023-03-27 17:10:32 +0300

    feat: add typification for action;

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,10 +1,32 @@
-import {TodolistsType} from "../App";
+import {FilterType, TodolistsType} from "../App";
 import {v1} from "uuid";
 
-type ActionType = {
-    type: string,
-    [key: string]: any,
+export type RemoveTodolistActionType = {
+    type: 'REMOVE-TODOLIST',
+    id: string
 }
+
+export type AddTodolistActionType = {
+    type: 'ADD-TODOLIST',
+    title: string
+}
+
+export type ChangeTodolistTitleActionType = {
+    type: 'CHANGE-TODOLIST-TITLE',
+    id: string
+    title: string
+}
+
+export type ChangeTodolistFilterActionType = {
+    type: 'CHANGE-TODOLIST-FILTER',
+    id: string
+    filter: FilterType
+}
+
+type ActionType = RemoveTodolistActionType |
+    AddTodolistActionType |
+    ChangeTodolistTitleActionType |
+    ChangeTodolistFilterActionType;
 
 
 export const todolistsReducer = (state: TodolistsType[], action: ActionType): TodolistsType[] => {
@@ -22,15 +44,11 @@ export const todolistsReducer = (state: TodolistsType[], action: ActionType): To
         }
 
         case 'CHANGE-TODOLIST-TITLE': {
-            const todolist = state.map(tl => tl.id === action.id ? {...tl, title: action.title} : tl);
-
-            return todolist
+            return state.map(tl => tl.id === action.id ? {...tl, title: action.title} : tl);
         }
 
         case 'CHANGE-TODOLIST-FILTER': {
-            const newFilter = state.map(tl => tl.id === action.id ? {...tl, filter: action.filter} : tl);
-
-            return newFilter
+            return state.map(tl => tl.id === action.id ? {...tl, filter: action.filter} : tl);
         }
 
         default:
