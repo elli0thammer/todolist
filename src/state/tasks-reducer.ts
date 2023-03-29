cd10ad58d1928935709a006516d7c353c885b075
Liza <richardaddy333@gmail.com>
2023-03-29 13:02:55 +0300

    feat: add reducer to change task;

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -14,8 +14,15 @@ export type AddTodolistActionType = {
     todolistId: string,
 }
 
+export type changeTaskStatusACType = {
+    type: 'CHANGE-TASK',
+    taskId: string,
+    isDone: boolean,
+    todolistId: string,
+}
+
 type ActionsType = RemoveTodolistActionType |
-    AddTodolistActionType ;
+    AddTodolistActionType | changeTaskStatusACType;
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
     switch (action.type) {
@@ -33,10 +40,18 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksS
             let newTask: TaskType = {idTask: v1(), title: action.title, isDone: false};
 
             const stateCopy = {...state}
-            let task = stateCopy[action.todolistId];
-            let taskObj = [newTask, ...task]
-            stateCopy[action.todolistId] = taskObj
+            let tasks = stateCopy[action.todolistId];
+            let newTasks = [newTask, ...tasks]
+            stateCopy[action.todolistId] = newTasks
 
+            return stateCopy
+        }
+
+        case "CHANGE-TASK": {
+            const stateCopy = {...state}
+            const newTasks = stateCopy[action.todolistId]
+            const newDone = newTasks.map(tl => tl.idTask === action.taskId ? {...tl, isDone: action.isDone} : tl);
+            stateCopy[action.todolistId] = newDone
             return stateCopy
         }
 
@@ -51,4 +66,8 @@ export const removeTaskAC = (taskId: string, todolistId: string): RemoveTodolist
 
 export const addTaskAC = (newTitle: string, todolistId: string): AddTodolistActionType => {
     return {type: "ADD-TASK", title: newTitle, todolistId}
+}
+
+export const changeTaskStatusAC = (taskId: string, isDone: boolean, todolistId: string): changeTaskStatusACType => {
+    return {type: 'CHANGE-TASK', taskId, isDone, todolistId}
 }
