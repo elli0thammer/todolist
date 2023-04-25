439a5d8e4027b752f4f7474d160860ff5481e738
Liza <richardaddy333@gmail.com>
2023-04-25 12:10:32 +0300

    feat: add story decorator

---
+++ src/state/ReduxStoreProviderDecorator.tsx
@@ -0,0 +1,8 @@
+import {Provider} from "react-redux";
+import { store } from "./store";
+
+export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactNode) => {
+    return <Provider store={store}>
+        {storyFn()}
+    </Provider>
+};
