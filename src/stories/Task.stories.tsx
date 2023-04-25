19170002ef5d029ba3ab6e66e374144da77316bd
Liza <richardaddy333@gmail.com>
2023-04-25 12:07:58 +0300

    feat: add storybook

---
+++ src/stories/Task.stories.tsx
@@ -0,0 +1,39 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {Task} from "../Task";
+import {action} from "@storybook/addon-actions";
+
+const meta: Meta<typeof Task> = {
+    title: 'Todolist/Task',
+    component: Task,
+    tags: ['autodocs'],
+    argTypes: {
+        changeStatus: {
+            description: 'changed task status',
+        },
+        changeTaskTitle: {
+            description: 'changed task title',
+        },
+        removeTask: {
+            description: 'removed task',
+        },
+    },
+    args: {
+        changeStatus: action('changed task status'),
+        changeTaskTitle: action('changed task title'),
+        removeTask: action('removed task'),
+        task: {idTask: '1', title: 'JS', isDone: true},
+        idToDo: '11'
+    }
+};
+
+export default meta;
+type Story = StoryObj<typeof Task>;
+
+export const TaskIsDoneStory: Story = {
+};
+
+export const TaskIsNotDoneStory: Story = {
+    args: {
+        task: {idTask: '1', title: 'Redux Toolkit', isDone: false},
+    },
+};
