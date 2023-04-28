18e0c37e89b3ad5abd98e06eb6a8c57bb105795c
Liza <richardaddy333@gmail.com>
2023-04-28 13:29:19 +0300

    feat: post request

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -6,7 +6,10 @@ export default {
 }
 
 const settings = {
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        "API-KEY": "c3a89d42-24e4-45a3-9a62-84b636c18679"
+    }
 }
 
 export const GetTodolists = () => {
@@ -16,7 +19,7 @@ export const GetTodolists = () => {
         let promise = axios.get("https://social-network.samuraijs.com/api/1.1/todo-lists", settings)
 
         promise.then((res) => {
-            return setState(res)
+            return setState(res.data)
         })
 
     }, [])
@@ -26,6 +29,12 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        let promise = axios.post("https://social-network.samuraijs.com/api/1.1/todo-lists",
+            { title: "Hello, buddy"  }, settings)
+
+        promise.then((res) => {
+            return setState(res.data)
+        })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
